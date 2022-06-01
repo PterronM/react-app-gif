0d2b77bcbd6edb27b4557d7b4202161f50a9d814
Pilar <pilarterronmartin@gmail.com>
2022-06-01 12:41:37 +0200

    docs agregado

---
+++ docs/precache-manifest.d4112bf3fbcb8deb2917ed258c786d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd95cb59eb8bf1d5ebf23e101338ba83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf48c8d1c6f4ca92cd35",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1b3548ece22ae56c7f06",
+    "url": "/static/js/2.0594b481.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.0594b481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf48c8d1c6f4ca92cd35",
+    "url": "/static/js/main.73747359.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
